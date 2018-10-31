ee4fbead80c646a054bbc21d7fcd698aef2ff2bc
Ayush Shirsat <43014839+Ayush-Shirsat@users.noreply.github.com>
2018-10-31 14:05:09 -0400

    Update read_image_seq.m

--- usr/AS/read_image_seq.m
+++ usr/AS/read_image_seq.m
@@ -2,11 +2,11 @@ tic
 clear all
 close all
 clc
-%imageDir = fullfile(toolboxdir('vision'), 'visiondata','upToScaleReconstructionImages');
-%images = imageDatastore(imageDir);
-I1 = imread('DJI_0828.JPG');
+
+% Read Images
+I1 = imread('img/DJI_0828.JPG');
 I1 = imresize(I1,[600 800]);
-I2 = imread('DJI_0829.JPG');
+I2 = imread('img/DJI_0829.JPG');
 I2 = imresize(I2,[600 800]);
 %figure
 imshowpair(I1, I2, 'montage');
@@ -18,7 +18,6 @@ imagePoints1 = detectMinEigenFeatures(rgb2gray(I1), 'MinQuality', 0.1);
 % Visualize detected points
 figure,
 imshow(I1, 'InitialMagnification', 50);
-title('Strongest Corners from the First Image');
 hold on
 plot(selectStrongest(imagePoints1, 500));
 
@@ -35,21 +34,20 @@ matchedPoints1 = imagePoints1(validIdx, :);
 matchedPoints2 = imagePoints2(validIdx, :);
 
 % Visualize correspondences
-figure
+figure,
 showMatchedFeatures(I1, I2, matchedPoints1, matchedPoints2);
 title('Tracked Features');
 
 % Estimate the fundamental matrix
 load upToScaleReconstructionCameraParameters.mat
-[E, epipolarInliers] = estimateEssentialMatrix(...
-    matchedPoints1, matchedPoints2, cameraParams, 'Confidence', 99.99);
+[E, epipolarInliers] = estimateEssentialMatrix(matchedPoints1, matchedPoints2, cameraParams, 'Confidence', 99.99);
 
 % Find epipolar inliers
 inlierPoints1 = matchedPoints1(epipolarInliers, :);
 inlierPoints2 = matchedPoints2(epipolarInliers, :);
 
 % Display inlier matches
-figure
+figure,
 showMatchedFeatures(I1, I2, inlierPoints1, inlierPoints2);
 title('Epipolar Inliers');
 
@@ -58,8 +56,7 @@ title('Epipolar Inliers');
 % Detect dense feature points. Use an ROI to exclude points close to the
 % image edges.
 roi = [30, 30, size(I1, 2) - 30, size(I1, 1) - 30];
-imagePoints1 = detectMinEigenFeatures(rgb2gray(I1), 'ROI', roi, ...
-    'MinQuality', 0.001);
+imagePoints1 = detectMinEigenFeatures(rgb2gray(I1), 'ROI', roi, 'MinQuality', 0.001);
 
 % Create the point tracker
 tracker = vision.PointTracker('MaxBidirectionalError', 1, 'NumPyramidLevels', 5);
@@ -88,8 +85,7 @@ points3D = triangulate(matchedPoints1, matchedPoints2, camMatrix1, camMatrix2);
 % Get the color of each reconstructed point
 numPixels = size(I1, 1) * size(I1, 2);
 allColors = reshape(I1, [numPixels, 3]);
-colorIdx = sub2ind([size(I1, 1), size(I1, 2)], round(matchedPoints1(:,2)), ...
-    round(matchedPoints1(:, 1)));
+colorIdx = sub2ind([size(I1, 1), size(I1, 2)], round(matchedPoints1(:,2)), round(matchedPoints1(:, 1)));
 color = allColors(colorIdx, :);
 
 % Create the point cloud
@@ -101,12 +97,10 @@ figure
 plotCamera('Size', cameraSize, 'Color', 'r', 'Label', '1', 'Opacity', 0);
 hold on
 grid on
-plotCamera('Location', loc, 'Orientation', orient, 'Size', cameraSize, ...
-    'Color', 'b', 'Label', '2', 'Opacity', 0);
+plotCamera('Location', loc, 'Orientation', orient, 'Size', cameraSize, 'Color', 'b', 'Label', '2', 'Opacity', 0);
 
 % Visualize the point cloud
-pcshow(ptCloud, 'VerticalAxis', 'y', 'VerticalAxisDir', 'down', ...
-    'MarkerSize', 45);
+pcshow(ptCloud, 'VerticalAxis', 'y', 'VerticalAxisDir', 'down', 'MarkerSize', 45);
 
 % Rotate and zoom the plot
 camorbit(0, -30);
@@ -117,5 +111,4 @@ xlabel('x-axis');
 ylabel('y-axis');
 zlabel('z-axis')
 
-title('Up to Scale Reconstruction of the Scene');
 toc
