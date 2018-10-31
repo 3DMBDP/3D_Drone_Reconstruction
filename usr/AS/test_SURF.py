cf54d1ef9aa013648cea489f9f7e98215f639004
Ayush Shirsat <43014839+Ayush-Shirsat@users.noreply.github.com>
2018-10-31 15:07:53 -0400

    Update test_SURF.py

--- usr/AS/test_SURF.py
+++ usr/AS/test_SURF.py
@@ -4,7 +4,6 @@ import numpy as np
 
 img1 = cv2.imread('/home/ayush34/Desktop/Project/seg1.png',0)
 img1 = cv2.resize(img1, (600, 800))
-img2 = cv2.imread('/home/ayush34/Desktop/Project/seg2.png',0)
 
 surf = cv2.xfeatures2d.SURF_create()
  
